9213633ef0bd99b8583cb50e9d255999363cd5e2
HasangaDineliKavindya <128205064+HasangaDineliKavindya@users.noreply.github.com>
2025-06-19 12:50:15 +0530

    Update db.js
    add

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
- host: 'cubik.cu-eflyer.top',
   user: 'cubikftp',
   password: 'LqWOGJB9PRmahmU',
   database: 'cubikftp',
